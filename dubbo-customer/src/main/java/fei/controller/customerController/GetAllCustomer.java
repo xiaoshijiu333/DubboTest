112027da3e17d0597bdae90cb296309a4db6a7de
xiaoshijiu <mychenshan@126.com>
2019-04-22 23:29:06 +0800

    swagger添加

--- dubbo-customer/src/main/java/fei/controller/customerController/GetAllCustomer.java
+++ dubbo-customer/src/main/java/fei/controller/customerController/GetAllCustomer.java
@@ -6,6 +6,8 @@ import fei.CustomerServices;
 import fei.model.Customer;
 import fei.utils.ApiResult;
 import fei.utils.BasePage;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @location DubboTest/fei
  * @date 2019/4/16
  */
+@Api(value = "Customer—Api")
 @RestController
 @RequestMapping("/Customer")
 public class GetAllCustomer {
@@ -26,6 +29,7 @@ public class GetAllCustomer {
     /**
      * @function: 获取所有的客户
      */
+    @ApiOperation(value = "查询所有客户")
     @RequestMapping(value = "/getAllCustomer", method = RequestMethod.GET)
     public ApiResult<Customer> getAllCustomer(BasePage basePage) {
         PageInfo<Customer> allCustomer = customerServices.getAllCustomer(basePage);
---
+++ dubbo-customer/src/main/java/fei/swagger/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package fei.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author xiaoshijiu
+ * @location dubbo2/fei.swagger
+ * @date 2019/4/21
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("fei.controller")) //接口所在包名
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("接口列表")
+                .termsOfServiceUrl("http://localhost:8080/swagger-ui.html")
+                .description("swagger 接口显示和测试")
+                .version("1.0")
+                .build();
+    }
+
+}
